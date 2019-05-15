f069cc9dc3e07d3e063d3a31ed19e70dd7afc0b7
lana-del-rey <iskandar@inbox.ru>
2019-05-15 13:34:00 +0300

    Flask mock file added

---
+++ flask_api.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import cherrypy
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return 'Face recognition and tracking API'
+
+
+
+def run_server():
+    # Enable WSGI access logging via Paste
+    crDict()
+    app_logged = TransLogger(app)
+
+    # Mount the WSGI callable object (app) on the root directory
+    cherrypy.tree.graft(app_logged, '/')
+
+    # Set the configuration of the web server
+    cherrypy.config.update({
+        'engine.autoreload_on': True,
+        'log.screen': True,
+        'server.socket_port': 5000,
+        'server.socket_host': '0.0.0.0'
+    })
+
+    # Start the CherryPy WSGI web server
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+if __name__ == "__main__":
+    run_server()
