0315087f94abd1aec5147cb70279694a319a30c0
lana-del-rey <iskandar@inbox.ru>
2019-05-20 19:03:48 +0300

    Discovery method added to API

--- flask_api.py
+++ flask_api.py
@@ -2,6 +2,7 @@ activate_this_file = "venv/bin/activate_this.py"
 execfile(activate_this_file, dict(__file__=activate_this_file))
 
 from flask import Flask, render_template, request
+from wsdiscovery import WSDiscovery
 import cherrypy
 from paste.translogger import TransLogger
 import configparser
@@ -18,6 +19,27 @@ config_file = '/home/ubuntu/MM.Tracker/conf/settings.ini'
 pid_file ='/home/ubuntu/MM.Tracker/log/pid'
 
 app = Flask(__name__)
+
+
+def discover():
+    # discovery method
+    try:
+        cameras = []
+        wsd = WSDiscovery()
+        wsd.start()
+        ret = wsd.searchServices()
+        print('Discovered {} services:'.format(len(ret)))
+        for i, service in enumerate(ret):
+            if 'onvif' in service.getXAddrs()[0]:
+                ip = service.getXAddrs()[0]
+                cameras.append(Camera(ip=ip))
+        wsd.stop()
+        return cameras
+
+    except Exception as e:
+        print(e)
+        print('Failed to discover services')
+        return
 
 @app.route('/')
 def homepage():
@@ -46,20 +68,22 @@ def tracking_url():
         if data['command'] == 'start':
             if len(pid_list) < 1:
                 ip = data['ip'] if 'ip' in data else None
-
                 port = data['port'] if 'port' in data else 80
+                login = data['login'] if 'login' in data else 'login'
+                password = data['password'] if 'password' in data else 'password'
 
                 config = configparser.ConfigParser()
                 config.read(config_file)
                 config['Settings']['ip'] = str(ip)
                 config['Settings']['port'] = str(port)
+                # TODO Save login and password from POST data to CONFIG
 
                 with open(config_file, 'w') as configfile:
                     config.write(configfile)
 
                 # Run Tracking Script
                 tracking_proc = subprocess.Popen('screen -S Tracking -dm bash -c "cd /home/ibakhtizin/ololo/MM.Tracker/; python test_scripts/test_classes.py;"', shell=True)
-                time.sleep(0.5)
+                time.sleep(0.1)
                 tracking_pid = tracking_proc.pid
                 print("TRACKING PID:", int(tracking_pid)+2)
 
@@ -68,6 +92,14 @@ def tracking_url():
                     f.write(str(int(tracking_pid)+2))
 
                 # TODO Run Recognition Script
+                recognition_proc = subprocess.Popen('screen -S Recognition -dm bash -c "python3 recognition_subprocess.py;";', shell=True)
+                time.sleep(0.1)
+                recognition_pid = recognition_proc.pid
+                print("RECOGNITION PID:", int(recognition_pid) + 2)
+
+                # Save recognition PID to file
+                with open(pid_file, 'a+') as f:
+                    f.write(str(int(recognition_pid) + 2))
 
 
                 """
@@ -81,12 +113,22 @@ def tracking_url():
                 # os.system('screen -S Tracking -dm bash -c "cd /home/ubuntu/MM.Tracker/; python test_scripts/test_classes.py;"')
                 # proc2 =
 
-                return 'Tracking started\nTracking PID: {}'.format(data, int(tracking_pid)+2)
+                return 'Tracking started' \
+                       '' \
+                       'Tracking PID: {tracking_pid}' \
+                       'Recognition PID: {recognition_pid}' \
+                       'Input: {data}'.format(
+                    tracking_pid=int(tracking_pid)+2,
+                    recognition_pid=int(recognition_pid) + 2,
+                    data=data
+                )
             else:
-                return 'Tracking already started with PID {}'.format(pid_list)
+                out = 'Tracking already started with PID {}'.format(pid_list)
+                print(out)
+                return out
 
         elif data['command'] == 'stop':
-
+            print("Stopping processes:", pid_list)
             for pid in pid_list:
                 os.kill(int(pid), signal.SIGTERM)
 
@@ -99,6 +141,15 @@ def tracking_url():
         return 'Tracking on/off method'
 
 
+@app.route('/results')
+def results():
+    return 'Face Recognition Results'
+
+
+@app.route('/discovery')
+def discovery():
+    cameras = discover()
+    return json.dumps(cameras)
 
 
 
