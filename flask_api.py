e79957ca6b9bf1cba9507a27aadf123aae98357a
lana-del-rey <iskandar@inbox.ru>
2019-05-24 18:31:39 +0300

    Folder name changed

--- flask_api.py
+++ flask_api.py
@@ -118,9 +118,7 @@ def tracking_url():
 
                 # # TODO Run Recognition Script
                 recognition_proc = subprocess.Popen(
-                    'screen -S Recognition -dm bash -c "cd /home/ubuntu/FaceRecognizer; '
-                    'source venv/bin/activate; '
-                    'python3 recognition_subprocess.py {folder}";'.format(
+                    'screen -S Recognition -dm bash -c "cd /home/ubuntu/FaceRecognizer; source venv/bin/activate; python3 recognition_subprocess.py {folder}";'.format(
                         folder='/home/ubuntu/MM.Tracker/recognition_queue/'
                     ),
                     shell=True
