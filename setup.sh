19c9622378778a67943c2f2ffd3750c7830f5a0d
Renamethis <ops282@mail.ru>
2020-11-09 19:42:41 +0300

    Setup optimized for server deploy

--- setup.sh
+++ setup.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
-os=($cat /etc/*-release)
-if ! grep "Fedora" $os 
+output=$(cat /etc/*-release)
+if ! echo $output  | grep "Ubuntu"
 then
-	str="apt-get"
-else
 	str="dnf"
+	sudo $str upgrade --refresh
+else
+	str="apt-get"
+	sudo $str install dnf-plugin-system-upgrade
 fi
-$str upgrade --refresh
-$str install dnf-plugin-system-upgrade
-$str install python2 
-python get-pip.py
-$str install -U virtualenv
-python2 setup.py $str
+sudo $str install virtualenv
+output=$(ls)
+if ! echo $output | grep "venv"
+then
+	echo "Venv error!"
+	exit 1
+fi
+echo "Venv is ok!"
+sudo virtualenv -p python2.7 ./venv
+cd venv
+cd bin
+./activate
+cd ..
+cd ..
+sudo python2 get-pip.py
+sudo pip install -r requirements.txt
+sudo wget "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz"
+sudo cp ssd_mobilenet_v2_coco_2018_03_29.tar.gz models/research/object_detection/
+#sudo python2 setup.py $str
